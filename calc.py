8c6cfbbfaee62fe656b5d6d819b0126486f1adaa
yoon-j <jiyoon.jeon258@gmail.com>
2024-07-10 16:56:03 +0900

    [feat] Add getMinus method of class Calc

--- calc.py
+++ calc.py
@@ -1,2 +1,3 @@
 class Calc:
-    pass
+    def getMinus(self, a, b):
+        return a - b
--- test_calc.py
+++ test_calc.py
@@ -1,5 +1,19 @@
 from unittest import TestCase
+from calc import Calc
 
 
 class TestCalc(TestCase):
-    pass
+
+    def setUp(self):
+        super().setUp()
+        self.sut = Calc()
+
+    def test_minus_a_larger_than_b(self):
+        self.assertEqual(3, self.sut.getMinus(7, 4))
+
+    def test_minus_a_equal_to_b(self):
+        self.assertEqual(0, self.sut.getMinus(3, 3))
+
+    def test_minusa_a_smaller_than_b(self):
+        self.assertEqual(-5, self.sut.getMinus(5, 10))
+
