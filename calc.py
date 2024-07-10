3f77717b64506210ba8aafb3fa7046fc875692a2
h.seok <h.seoK@samsung.com>
2024-07-10 16:49:08 +0900

    complete divide module

--- calc.py
+++ calc.py
@@ -1,2 +1,10 @@
 class Calc:
     pass
+
+    def getDivide(self, a, b):
+        self.validation_of_divider(b)
+        return a / b
+
+    def validation_of_divider(self, b):
+        if (b == 0):
+            raise ValueError("Divider must not be zero")
--- test_calc.py
+++ test_calc.py
@@ -1,5 +1,28 @@
 from unittest import TestCase
 
+from calc import Calc
+
 
 class TestCalc(TestCase):
-    pass
+    def setUp(self):
+        super().setUp()
+        self.sut = Calc()
+
+    def test_getDivide_should_raise_exception_when_second_arg_is_zero(self):
+        a = 1
+        b = 0
+
+        with self.assertRaises(ValueError) as ve:
+            self.sut.getDivide(a, b)
+        self.assertEqual("Divider must not be zero", str(ve.exception))
+
+    def test_getDivide_should_return_correct_value(self):
+        a = 4
+        b = 2
+        expected_result = 2
+        self.assertEqual(expected_result, self.sut.getDivide(a, b))
+
+        a = -4
+        b = 2
+        expected_result = -2
+        self.assertEqual(expected_result, self.sut.getDivide(a, b))
