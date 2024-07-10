b3063087644e968a87b34fb12db7308ba6b1ba85
geniuskey <geniuskey@gmail.com>
2024-07-10 16:53:40 +0900

    add calc_sum_sum

--- calc.py
+++ calc.py
@@ -1,2 +1,5 @@
 class Calc:
     pass
+
+    def calc_sum_sum(self, a, b, c):
+        return a + b + c
--- test_calc.py
+++ test_calc.py
@@ -1,5 +1,14 @@
 from unittest import TestCase
+from calc import Calc
 
 
 class TestCalc(TestCase):
-    pass
+
+    def test_calc_sum_sum(self):
+        calc = Calc()
+        for a, b, c, ans in [(1, 1, 1, 3),
+                             (1, 2, 3, 6),
+                             (2, 2, 2, 6)]:
+            with self.subTest(f"{a}+{b}+{c}={ans}"):
+                ret = calc.calc_sum_sum(a, b, c)
+                self.assertEqual(ret, ans)
